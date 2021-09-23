ac5fbc26abf11814332e57de1b54e655dd71363b
Divinatge21 <55342393+Divinatge21@users.noreply.github.com>
2021-09-23 09:57:02 +0500

    HW

---
+++ Roll-a-Ball/Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour
+{
+
+	
+	void Update()
+	{
+	
+		transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+	}
+}
